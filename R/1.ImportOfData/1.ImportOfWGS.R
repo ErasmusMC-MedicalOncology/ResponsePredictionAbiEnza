450f04d6425961f727c96e5b3792d93291b30ea7
J0bbie <j.vanriet@erasmusmc.nl>
2021-03-30 17:20:01 +0200

    Sync

--- R/1.ImportOfData/1.ImportOfWGS.R
+++ R/1.ImportOfData/1.ImportOfWGS.R
@@ -12,8 +12,64 @@ library(ShatterSeek)
 R2CPCT::initializeLogger(file = '~/AbiEnza.log')
 
 # Load metadata of the Abi/Enza-treated patients.
-AbiEnza.Metadata <- readxl::read_xlsx('Misc/SupTable1_OverviewOfData.xlsx', sheet = 'Sample overview')
+AbiEnza.Metadata <- readxl::read_xlsx('Misc/Suppl. Table 1 - OverviewOfData.xlsx', sheet = 'Sample overview')
+
+# Import / convert WGS samples --------------------------------------------
+
+# Import the WGS data of all samples in the cohort.
+AbiEnza.CohortWGS <- R2CPCT::importWGSOfCohort(AbiEnza.Metadata$sampleId, '/mnt/data2/hartwig/DR71/Oct2020/dataHMF/combinedData/', nThreads = 20)
+save(AbiEnza.CohortWGS, file = '/mnt/data2/hartwig/DR71/Oct2020_AbiEnza/RData/AbiEnza.CohortWGS.RData')
 
 
-# Retrieve all relevant HMF files -----------------------------------------
+# Perform GISTIC2 analysis ------------------------------------------------
 
+# Generate the commands to perform GISTIC2 (perform this in your Bash terminal).
+# Perform this separately per responder group (Good / Poor).
+
+# All samples.
+R2CPCT::performGISTIC2(AbiEnza.CohortWGS$copyNumbers, outputFolder = '/mnt/data2/hartwig/DR71/Oct2020_AbiEnza/results/GISTIC2/AllSamples/')
+
+# Good responders.
+R2CPCT::performGISTIC2(AbiEnza.CohortWGS$copyNumbers[GenomicRanges::mcols(AbiEnza.CohortWGS$copyNumbers)$sample %in% (AbiEnza.Metadata %>% dplyr::filter(grepl('Good Responder', responderCategory)) %>% dplyr::pull(sampleId)),], outputFolder = '/mnt/data2/hartwig/DR71/Oct2020_AbiEnza/results/GISTIC2/GoodResponders/')
+
+# Poor responders.
+R2CPCT::performGISTIC2(AbiEnza.CohortWGS$copyNumbers[GenomicRanges::mcols(AbiEnza.CohortWGS$copyNumbers)$sample %in% (AbiEnza.Metadata %>% dplyr::filter(grepl('Poor Responder', responderCategory)) %>% dplyr::pull(sampleId)),], outputFolder = '/mnt/data2/hartwig/DR71/Oct2020_AbiEnza/results/GISTIC2/PoorResponders/')
+
+# GISTIC2 command used:
+# GISTIC2_2.0.23/gistic2 -b /mnt/data2/hartwig/DR71/Oct2020_AbiEnza/results/GISTIC2/PoorResponders/ -seg /tmp/Rtmp7hzfek/50de7e5bd09e.txt -refgene GISTIC2_2.0.23/refgenefiles/hg19.UCSC.add_miR.140312.refgene.mat -genegistic 1 -gcm extreme -maxseg 4000 -broad 1 -brlen 0.98 -conf 0.95 -rx 0 -cap 3 -saveseg 0 -armpeel 1 -smallmem 0 -res 0.01 -ta 0.3 -td 0.3 -savedata 0 -savegene 1 -qvt 0.1 -twoside 0
+
+
+# Cohort-wide analysis ----------------------------------------------------
+
+# Initialize a list to contain the results of the WGS analysis.
+AbiEnza.Results <- list()
+
+# Generate overview of mutational burden per sample and cohort.
+AbiEnza.Results$mutationalBurden <- R2CPCT::determineMutationalBurden(AbiEnza.CohortWGS, minTAF.Muts = 0, minTAF.SV = 0)
+
+# Perform dN/dS on entire cohort and per responder groups.
+AbiEnza.Results$dNdS <- R2CPCT::rundNdS(AbiEnza.CohortWGS$somaticVariants)
+AbiEnza.Results$dNdS.GoodResponders <- R2CPCT::rundNdS(AbiEnza.CohortWGS$somaticVariants[AbiEnza.CohortWGS$somaticVariants$sample %in% (AbiEnza.Metadata %>% dplyr::filter(grepl('Good Responder', responderCategory)) %>% dplyr::pull(sampleId)),])
+AbiEnza.Results$dNdS.PoorResponders <- R2CPCT::rundNdS(AbiEnza.CohortWGS$somaticVariants[AbiEnza.CohortWGS$somaticVariants$sample %in% (AbiEnza.Metadata %>% dplyr::filter(grepl('Poor Responder', responderCategory)) %>% dplyr::pull(sampleId)),])
+
+# Import the GISTIC2-determined recurrent CNA peaks.
+AbiEnza.Results$GISTIC2.AllSamples <- R2CPCT::importGISTIC2('/mnt/data2/hartwig/DR71/Oct2020_AbiEnza/results/GISTIC2/AllSamples/')
+AbiEnza.Results$GISTIC2.GoodResponders <- R2CPCT::importGISTIC2('/mnt/data2/hartwig/DR71/Oct2020_AbiEnza/results/GISTIC2/GoodResponders/')
+AbiEnza.Results$GISTIC2.PoorResponders <- R2CPCT::importGISTIC2('/mnt/data2/hartwig/DR71/Oct2020_AbiEnza/results/GISTIC2/PoorResponders/')
+
+# Generate the gene-level mutational overview.
+AbiEnza.Results$combinedReport <- R2CPCT::generateCombinedReport(AbiEnza.CohortWGS, dNdS = AbiEnza.Results$dNdS, GISTIC2 = AbiEnza.Results$GISTIC2.AllSamples, nThreads = 20, mutantsOnly = T)
+
+# Determine mutational signatures.
+AbiEnza.Results$mutSigs <- R2CPCT::fitMutSigs(AbiEnza.CohortWGS$somaticVariants, restrictiveFit = F)
+
+# Determine # of Ti/Tv and ratio per sample.
+AbiEnza.Results$TiTv <- R2CPCT::determineTiTv(AbiEnza.Results$mutSigs$SNV$mutMatrix)
+
+# Save results.
+save(AbiEnza.Results, file = '/mnt/data2/hartwig/DR71/Oct2020_AbiEnza/RData/AbiEnza.Results.RData')
+
+
+# Close logger ------------------------------------------------------------
+
+ParallelLogger::unregisterLogger()
---
+++ R/1.ImportOfData/3.ImportOfRNA.R
@@ -0,0 +1,73 @@
+# Author:    Job van Riet
+# Date:      30-03-21
+# Function:  Import and processing of the Abi/Enza-treated RNA-Seq samples (DR-071).
+
+# Libraries ---------------------------------------------------------------
+
+library(R2CPCT)
+library(DESeq2)
+
+# Load metadata of the Abi/Enza-treated patients.
+AbiEnza.Metadata <- readxl::read_xlsx('Misc/Suppl. Table 1 - OverviewOfData.xlsx', sheet = 'Sample overview')
+
+# Clean-up the responder category for use in DESEq2.
+AbiEnza.Metadata <- AbiEnza.Metadata %>% dplyr::mutate(responderCategory.DESeq2 = gsub(' .*', '', responderCategory))
+
+
+# Retrieve batch-effect genes ---------------------------------------------
+
+load('/mnt/data2/hartwig/DR71/Oct2020/RData/DR71.RNASeq.RData')
+
+## Retrieve batch-effect genes ----
+diffGenes.Batch <- DR71.RNASeq$DESeq2Results.BetweenMajorBiopsySite %>% dplyr::filter(padj <= 0.05, lfcSE <= 1, abs(log2FoldChange) >= 1) %>% dplyr::distinct(ENSEMBL)
+
+
+# Import read-counts ------------------------------------------------------
+
+# Import gene-annotations.
+geneInfo <- R2CPCT::GENCODE.v35
+geneInfo <- tibble::as_tibble(S4Vectors::mcols(geneInfo))
+
+# Only retain protein-coding genes.
+geneInfo <- geneInfo %>% dplyr::filter(gene_type == 'protein_coding')
+geneInfo <- geneInfo %>% dplyr::distinct(SYMBOL, ENSEMBL)
+
+# Remove batch-effect genes.
+geneInfo <- geneInfo %>% dplyr::filter(!ENSEMBL %in% diffGenes.Batch$ENSEMBL)
+
+# Import counts of the entire DR-071 cohort.
+counts <- readr::read_delim('/mnt/data2/hartwig/DR71/Oct2020/dataHMF/RNASeq/counts/DR71_RNA.counts', delim = '\t', comment = '#')
+colnames(counts) <- gsub('_.*', '', gsub('.*/', '', colnames(counts)))
+
+# Convert to matrix.
+countMatrix <- as.matrix(counts[7:ncol(counts)])
+rownames(countMatrix) <- counts$Geneid
+
+# Subset Abi/Enza samples.
+countMatrix <- countMatrix[,colnames(countMatrix) %in% AbiEnza.Metadata$sampleId]
+
+# Subset genes.
+countMatrix <- countMatrix[rownames(countMatrix) %in% geneInfo$ENSEMBL,]
+
+
+# Generate DESeq2 Dataset -------------------------------------------------
+
+DESeq2Counts.AbiEnza <- DESeq2::DESeqDataSetFromMatrix(countData = countMatrix, colData = AbiEnza.Metadata[match(colnames(countMatrix), AbiEnza.Metadata$sampleId),], design = ~responderCategory.DESeq2)
+SummarizedExperiment::rowData(DESeq2Counts.AbiEnza)$ENSEMBL <- BiocGenerics::rownames(DESeq2Counts.AbiEnza)
+SummarizedExperiment::rowData(DESeq2Counts.AbiEnza) <- tibble::as_tibble(SummarizedExperiment::rowData(DESeq2Counts.AbiEnza)) %>% dplyr::left_join(tibble::as_tibble(S4Vectors::mcols(R2CPCT::GENCODE.v35)) %>% dplyr::distinct(SYMBOL, ENSEMBL), by = 'ENSEMBL')
+
+save(DESeq2Counts.AbiEnza, file = '/mnt/data2/hartwig/DR71/Oct2020_AbiEnza/RData/DESeq2Counts.AbiEnza.RData')
+
+
+# Perform DESeq2 ----------------------------------------------------------
+
+DESeq2.AbiEnza <- DESeq2::DESeq(DESeq2Counts.AbiEnza, test = 'Wald', parallel = T, BPPARAM = BiocParallel::MulticoreParam(workers = 20))
+
+# Retrieve the results. (Poor vs. Good responders)
+DESeq2.AbiEnza.Results <- R2CPCT::retrieveDESeq2Results(DESeq2.AbiEnza, contrast = c('responderCategory.DESeq2', 'Poor', 'Good'))
+
+# Retrieve Diff. Exprs. genes (DE).
+DESeq2.AbiEnza.Results <- DESeq2.AbiEnza.Results %>% 
+  dplyr::mutate(isSig = ifelse(padj <= 0.05 & lfcSE <= 1 & baseMean >= 50 & abs(log2FoldChange) >= 0.5, 'Significant', 'Not Significant'))
+
+DESeq2.AbiEnza.Results %>% dplyr::filter(isSig == 'Significant')
