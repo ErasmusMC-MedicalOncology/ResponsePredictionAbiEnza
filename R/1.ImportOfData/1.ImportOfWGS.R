bdd559030d018a99766411908ce3dc07ea05d1a7
J0bbie <jobvriet@gmail.com>
2022-12-12 13:44:40 +0000

    Added exonic TMB

--- R/1.ImportOfData/1.ImportOfWGS.R
+++ R/1.ImportOfData/1.ImportOfWGS.R
@@ -43,7 +43,7 @@ R2CPCT::performGISTIC2(AbiEnza.CohortWGS$copyNumbers[GenomicRanges::mcols(AbiEnz
 
 # GISTIC2 command used:
 # Repeated samples in separate analysis
- 
+
 #sudo docker run -it --rm -v /mnt/share1/repository/HMF/DR71/Dec2021/results/GISTIC2/PredictionOfAbiEnza/:/mnt/share1/repository/HMF/DR71/Dec2021/results/GISTIC2/PredictionOfAbiEnza/ -v /tmp/:/tmp/ --entrypoint bash shixiangwang/gistic gistic2 -b /mnt/share1/repository/HMF/DR71/Dec2021/results/GISTIC2/PredictionOfAbiEnza/AllSamples/ -seg /tmp/RtmpyYjjN7/3ae358130a68.txt -refgene refgenefiles/hg19.UCSC.add_miR.140312.refgene.mat -genegistic 1 -gcm extreme -maxseg 4000 -broad 1 -brlen 0.98 -conf 0.95 -rx 0 -cap 3 -saveseg 0 -armpeel 1 -smallmem 0 -res 0.01 -ta 0.3 -td 0.3 -savedata 0 -savegene 1 -qvt 0.1 -twoside 0
 #sudo docker run -it --rm -v /mnt/share1/repository/HMF/DR71/Dec2021/results/GISTIC2/PredictionOfAbiEnza/:/mnt/share1/repository/HMF/DR71/Dec2021/results/GISTIC2/PredictionOfAbiEnza/ -v /tmp/:/tmp/ --entrypoint bash shixiangwang/gistic gistic2 -b /mnt/share1/repository/HMF/DR71/Dec2021/results/GISTIC2/PredictionOfAbiEnza/GoodResponders/ -seg /tmp/RtmpaJ7Bk5/4eea3d339bc9.txt -refgene refgenefiles/hg19.UCSC.add_miR.140312.refgene.mat -genegistic 1 -gcm extreme -maxseg 4000 -broad 1 -brlen 0.98 -conf 0.95 -rx 0 -cap 3 -saveseg 0 -armpeel 1 -smallmem 0 -res 0.01 -ta 0.3 -td 0.3 -savedata 0 -savegene 1 -qvt 0.1 -twoside 0
 #sudo docker run -it --rm -v /mnt/share1/repository/HMF/DR71/Dec2021/results/GISTIC2/PredictionOfAbiEnza/:/mnt/share1/repository/HMF/DR71/Dec2021/results/GISTIC2/PredictionOfAbiEnza/ -v /tmp/:/tmp/ --entrypoint bash shixiangwang/gistic gistic2 -b /mnt/share1/repository/HMF/DR71/Dec2021/results/GISTIC2/PredictionOfAbiEnza/BadResponders/ -seg /tmp/RtmpaJ7Bk5/4eeac42c45c.txt -refgene refgenefiles/hg19.UCSC.add_miR.140312.refgene.mat -genegistic 1 -gcm extreme -maxseg 4000 -broad 1 -brlen 0.98 -conf 0.95 -rx 0 -cap 3 -saveseg 0 -armpeel 1 -smallmem 0 -res 0.01 -ta 0.3 -td 0.3 -savedata 0 -savegene 1 -qvt 0.1 -twoside 0
@@ -56,6 +56,21 @@ AbiEnza.Results <- list()
 
 # Generate overview of mutational burden per sample and cohort.
 AbiEnza.Results$mutationalBurden <- R2CPCT::determineMutationalBurden(AbiEnza.CohortWGS, minTAF.Muts = 0, minTAF.SV = 0)
+
+# Determine exonic TMB.
+data(GENCODE.v38, package = 'R2CPCT')
+GENCODE.v38.proteinCoding <- GENCODE.v38[GENCODE.v38$gene_type == 'protein_coding']
+GENCODE.v38.proteinCoding <- GenomicRanges::reduce(GENCODE.v38.proteinCoding)
+
+exonMuts <- IRanges::subsetByOverlaps(AbiEnza.CohortWGS$somaticVariants, GENCODE.v38.proteinCoding) %>% 
+    tibble::as_tibble(S4Vectors::mcols(.)) %>% 
+    dplyr::group_by(sample) %>% 
+    dplyr::summarise(
+        Exome.TMB = dplyr::n() / (sum(GenomicRanges::width(GENCODE.v38.proteinCoding))/1e+06)
+    ) %>% 
+    dplyr::ungroup()
+
+AbiEnza.Results$mutationalBurden <- AbiEnza.Results$mutationalBurden %>% dplyr::inner_join(exonMuts)
 
 # Perform dN/dS on entire cohort and per responder groups.
 AbiEnza.Results$dNdS <- R2CPCT::rundNdS(AbiEnza.CohortWGS$somaticVariants)
